4a51e5fd2be326b6fd0e205167a050dcaf26763c
Junichi Tokuda <tokuda@bwh.harvard.edu>
2013-06-21 10:13:12 -0400

    STYLE: Clean up the code

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ CommunicationTest
 =================
 
 Command line simulator software to perform communication test between navigation software and robot control software.
+
+For the detail of the testing protocol, please see:
+
+http://wiki.na-mic.org/Wiki/index.php/ProstateBRP_OpenIGTLink_Communication_June_2013
--- TestSimulatorServer.cxx
+++ TestSimulatorServer.cxx
@@ -9,6 +9,10 @@
   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
   PURPOSE.  See the above copyright notices for more information.
 
+  Please see
+    http://wiki.na-mic.org/Wiki/index.php/ProstateBRP_OpenIGTLink_Communication_June_2013
+  for the detail of the testing protocol.
+
 =========================================================================*/
 
 #include <iostream>
